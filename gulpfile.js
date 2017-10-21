4350fe34ed9efb1581c884502c1b9ee70bc12c05
Ge11ert <gellert94@mail.ru>
2017-10-21 18:15:38 +0300

    новые таски, спрайт и оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,21 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var posthtml = require("gulp-posthtml");
+var includeHtml = require("posthtml-include");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var uglify = require("gulp-uglify");
+var htmlmin = require("gulp-htmlmin");
+var concat = require("gulp-concat");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rsp = require("remove-svg-properties").stream;
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -16,6 +29,34 @@ gulp.task("style", function() {
     ]))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("webp", function () {
+  return gulp.src("img/**/*.{jpg,png}")
+    .pipe(webp({quality: 85}))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("img/*.svg")
+    .pipe(svgstore({
+        inlineSvg: true
+      }))
+    .pipe(rename("sprite.svg"))
+    .pipe(rsp.remove({
+        properties: [rsp.PROPS_FILL]
+      }))
+    .pipe(gulp.dest("img"));
 });
 
 gulp.task("serve", ["style"], function() {
